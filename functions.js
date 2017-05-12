4bbb890bd3f65781b12e80d05e059ed3b554ebde
Steve Larsen <stevelarsen01@gmail.com>
2017-05-12 14:10:40 -0500

    solved

--- functions.js
+++ functions.js
@@ -1,1 +1,11 @@
 /*jshint esversion: 6 */
+function pigIt(str){
+  var array = str.split(" ");
+  for (var i = 0; i < array.length; i++) {
+    array[i] = (array[i].slice(1,array[i].length)) + array[i].charAt(0) + "ay";
+  }
+  return array.join(" ");
+}
+
+console.log(pigIt('Pig latin is cool'),'igPay atinlay siay oolcay');
+console.log(pigIt('This is my string'),'hisTay siay ymay tringsay');
